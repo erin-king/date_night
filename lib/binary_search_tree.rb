cc36cdf5b08b875a5b0b01b7a3acdf8ddd6780c4
Erin King <44042415+erin-king@users.noreply.github.com>
2019-05-10 11:15:06 -0600

    Time for the challenge is up- add Node and BinarySearchTree classes, started following tutorial, see reference in binary_search_tree.rb

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,3 +1,34 @@
-class BinarySearchTree
+#After an hour of trying, I am going to do this tutorial style to see if I can understand the structure better.
+# Source: https://medium.com/@jessgreb01/data-structures-trees-and-ruby-2959c47ffa26
 
+class Node
+  attr_reader :value
+  attr_accessor :left,
+               :right
+
+  def initialize(value = nil)
+    @value = value
+    left = nil
+    right = nil
+  end
+end
+
+class BinarySearchTree
+  attr_accessor :root_node
+
+  def initialize(root_value = nil)
+    @root_node = Node.new(root_value)
+  end
+
+  def insert(node, value)
+    if node.value == value
+      return node
+    elsif value < node.value
+      insert(node.right, value)
+    end
+  end
+
+# 1.check if there is a root node
+# 2. traverse tree until the end
+# 3. if there is a duplicate value, stop operation 4. insert new node
 end
